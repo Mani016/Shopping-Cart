5cf7fdb7d3ccb6774faff22298b4b933120db53f
Mani016 <manishagupta016@gmail.com>
2019-09-08 13:01:36 +0530

    Shopping Cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { ProductService } from './service/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+   routing,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
